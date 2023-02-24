259d6f299c561eb6330bbb9da8d62df3a0c7873d
Dmitry <theotheo46@gmail.com>
2023-02-24 23:48:36 +0300

    convert js to ts

--- src/hbs_picture_css.d.ts
+++ src/hbs_picture_css.d.ts
@@ -2,7 +2,10 @@ declare module '*.png'
 declare module '*.pcss'
 
 declare module "*.hbs" {
-    import { TemplateDelegate } from "handlebars/runtime";
+    import { TemplateDelegate } from "handlebars";
     const template : TemplateDelegate;
     export default template;
 }
+
+declare module 'handlebars/dist/handlebars.runtime';
+ 
