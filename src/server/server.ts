da1e37d88661b28f34994ede9926eca3690663cc
Dmitry <theotheo46@gmail.com>
2023-02-28 22:38:05 +0300

    linter fix

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,7 @@
-import express = require('express');
-import path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 const PORT = 3000;
 const app = express();
 app.use(express.static(path.join(__dirname, '../../dist')));
