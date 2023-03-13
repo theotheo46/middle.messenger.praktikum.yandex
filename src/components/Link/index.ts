606d853583150e1964d020afaf6609265bd1fa1f
theo <theotheo46@gmail.com>
2023-03-13 16:21:53 +0300

    some files are added

--- src/components/Link/index.ts
+++ src/components/Link/index.ts
@@ -3,16 +3,28 @@ import template from './link.hbs';
 import * as styles from '../../styles.module.pcss';
 
 export interface LinkProps {
-  label?: string;
-  href : string;
   image? : unknown;
   name? : string;
   alt? : string;
+  to: string;
+  label: string;
+  events?: {
+    click: () => void;
+  };
+
 }
 
 export class Link extends Block<LinkProps> {
   constructor(props: LinkProps) {
-    super(props);
+    super({...props,
+      events: {
+        click: () => this.navigate()
+      },
+    });
+  }
+
+  navigate() {
+    console.log('In navigate');
   }
 
   render() {
